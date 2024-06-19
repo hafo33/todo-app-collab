566e86be65983f95e33a04657327439e6edbb146
Hugo Forero <tu.hafo33@gmail.com>
2024-06-19 13:01:53 -0500

    Initial Commit

---
+++ src/main/java/com/linktic/todoapp/TodoAppCollabApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppCollabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/todoapp/TodoAppCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
